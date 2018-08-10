5999646d1a7bfbc21d5d0eae80f0a6132cebafdc
Tiep Vu <vuhuutiep@gmail.com>
2018-08-09 20:09:22 -0400

    first commit

---
+++ inference.py
@@ -0,0 +1,113 @@
+from __future__ import division
+from zalo_utils import *
+from sklearn.model_selection import train_test_split
+from utils import *
+import torch
+import torch.nn as nn
+import argparse
+import copy
+import random
+from torchvision import transforms
+# import time
+import torch.backends.cudnn as cudnn
+import os, sys
+from time import time, strftime
+# from nets import load_model, parallelize_model
+
+parser = argparse.ArgumentParser(description='PyTorch Digital Mammography Training')
+parser.add_argument('--net_type', default='resnet', type=str, help='model')
+parser.add_argument('--depth', default=18, type=int, help='depth of model')
+parser.add_argument('--model_path', type=str, default = ' ')
+parser.add_argument('--batch_size', type=str, default = 512)
+
+parser.add_argument('--frozen_until', '-fu', type=int, default=7,
+                    help="freeze until --frozen_util block")
+parser.add_argument('--val_ratio', default=0.1, type=float, 
+        help = "number of training samples per class")
+args = parser.parse_args()
+
+KTOP = 3 # top k error
+
+data_dir = '../data/Public/'
+fn_all = [data_dir + fn for fn in os.listdir(data_dir) if fn.endswith('.jpg')]
+fns = []
+for fn in fn_all:
+    # filter dammaged images
+    if os.path.getsize(fn) > 0:
+        fns.append(fn)
+
+print('Total provided files: {}'.format(len(fn_all)))
+print('Total damaged files: {}'.format(len(fn_all) - len(fns)))
+
+lbs = [-1]*len(fns)
+
+mean=[0.485, 0.456, 0.406]
+std=[0.229, 0.224, 0.225]
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        transforms.RandomHorizontalFlip(),  # simple data augmentation
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+        ]),
+    'val': transforms.Compose([
+        transforms.Scale(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+}
+
+dsets = dict()
+dsets['test'] = MyDataset(fns, lbs, transform=data_transforms['val'])
+
+dset_loaders = {
+    x: torch.utils.data.DataLoader(dsets[x],
+                                   batch_size=args.batch_size,
+                                   shuffle= False,
+                                   num_workers=8)
+    for x in ['test']
+}
+
+#################
+# load model 
+old_model = './checkpoint/' + 'resnet' + '-%s' % (args.depth) + '_' + args.model_path + '.t7'
+print("| Load pretrained at  %s..." % old_model)
+checkpoint = torch.load(old_model, map_location=lambda storage, loc: storage)
+tmp = checkpoint['model']
+# model = 
+model = unparallelize_model(tmp)
+model = parallelize_model(model)
+best_top3 = checkpoint['top3']
+print('previous top3\t%.4f'% best_top3)
+print('=============================================')
+
+res_fn = 'mysubmission.csv'
+f = open(res_fn, 'w')
+header = 'id,predicted\n'
+f.write(header)
+torch.set_grad_enabled(False)
+model.eval()
+k = 3 # top 3 erro
+tot = 0 
+for batch_idx, (inputs, labels, fns0) in enumerate(dset_loaders['test']):
+    inputs = cvt_to_gpu(inputs)
+    # labels = cvt_to_gpu(labels)
+    #1 outputs = model.predict(inputs, k=3)
+    # pdb.set_trace()
+    outputs = model(inputs)
+    outputs = outputs.data.cpu().numpy()
+    outputs = np.argsort(outputs, axis = 1)[:, -k:][:, ::-1]
+    ########
+    # write to file
+    for i in range(len(fns0)):
+        idx = fns0[i].split('/')[-1][:-4]
+        # print(idx)
+        tmp = idx + ','+ str(outputs[i, 0] )+ ' ' + str(outputs[i, 1]) + ' ' + str(outputs[i, 2]) +  '\n'
+        f.write(tmp)
+    tot += len(fns0)
+    print('processed {}/{}'.format(tot, len(fns)))
+
+f.close()
+print('done')
---
+++ zalo_utils.py
@@ -0,0 +1,243 @@
+from __future__ import print_function 
+import os
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+import torchvision.models as models
+import torch
+import torch.nn as nn
+import pickle
+import pdb
+import torch.optim as optim
+from PIL import Image
+import numpy as np
+import random
+import torch.backends.cudnn as cudnn
+from time import time
+
+def get_fns_lbs(base_dir, json_file, pickle_fn = 'mydata.p', force = False):    
+    pickle_fn = base_dir + pickle_fn 
+    # pdb.set_trace() 
+    if os.path.isfile(pickle_fn) and not force:
+        mydata = pickle.load(open(pickle_fn, 'rb'))
+        fns = mydata['fns']
+        lbs = mydata['lbs']
+        cnt = mydata['cnt']
+        return fns, lbs, cnt
+
+    f = open(json_file, 'r')
+    line = f.readlines()[0] # only one line 
+    
+    end = 0 
+    id_marker = '\\"id\\": '
+    cate_marker = '\\"category\\": '
+    cnt = 0 
+    fns = [] # list of all image filenames
+    lbs = [] # list of all labels
+    while True:
+        start0 = line.find(id_marker, end)
+        if start0 == -1: break 
+        start_id = start0 + len(id_marker)
+        end_id = line.find(',', start_id) 
+
+        start0 = line.find(cate_marker, end_id)
+        start_cate = start0 + len(cate_marker)
+        end_cate = line.find('}', start_cate)
+
+        end = end_cate
+        cnt += 1
+        cl = line[start_cate:end_cate]
+        fn = base_dir + cl + '/' + line[start_id:end_id] + '.jpg'
+        if os.path.getsize(fn) == 0: # zero-byte files 
+            continue 
+        lbs.append(int(cl))
+        fns.append(fn)
+
+    # pdb.set_trace()
+    mydata = {'fns':fns, 'lbs':lbs, 'cnt':cnt}
+    pickle.dump(mydata, open(pickle_fn, 'wb'))
+    print(os.path.isfile(pickle_fn))
+
+    return fns, lbs, cnt 
+
+class MyDataset(Dataset):
+
+    def __init__(self, filenames, labels, transform=None):
+        assert len(filenames) == len(labels), "Number of files != number of labels"
+        self.fns = filenames
+        self.lbs = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.fns)
+
+    def __getitem__(self, idx):
+        # TODO: replace Image by opencv
+        image = Image.open(self.fns[idx])
+        tmp = image.getpixel((0, 0))
+        if isinstance(tmp, int) or len(tmp) != 3: # not rgb image
+            image = image.convert("RGB")
+        # image = Image.fromarray(cv2.imread(self.fns[idx], 1)[:, :, ::-1]) # color
+        if self.transform:
+            image = self.transform(image)
+        return image, self.lbs[idx], self.fns[idx]
+
+
+# def buildDataLoader(args):
+#     data_transforms = {
+# 	'train': transforms.Compose([
+# 	    transforms.RandomSizedCrop(224),
+# 	    transforms.RandomHorizontalFlip(),
+# 	    transforms.ToTensor(),
+# 	    transforms.Normalize(cf.mean, cf.std)
+# 	]),
+# 	'val': transforms.Compose([
+# 	    transforms.Scale(224),
+# 	    transforms.CenterCrop(224),
+# 	    transforms.ToTensor(),
+# 	    transforms.Normalize(cf.mean, cf.std)
+# 	]),
+#     }
+
+#     (train_filenames, train_labels, val_filenames, val_labels,
+#          retrieved_src_fns, retrieved_src_labels) = fns_and_labels(args)
+
+#     dsets = dict()
+#     dsets['train'] = MyDataset(train_filenames, train_labels, transform=data_transforms['train'])
+#     dsets['val'] = MyDataset(val_filenames, val_labels, transform=data_transforms['val'])
+#     dsets['src'] = MyDataset(retrieved_src_fns, retrieved_src_labels, transform=data_transforms['train'])
+
+#     dset_loaders = {
+#         x: torch.utils.data.DataLoader(dsets[x],
+#                                        batch_size=args.batch_size,
+#                                        shuffle=(x != 'val'),
+#                                        num_workers=8)
+#         for x in ['train', 'val', 'src']
+#     }
+
+
+class MyResNet(nn.Module):
+    def __init__(self, depth, num_classes, pretrained = True):
+        super(MyResNet, self).__init__()
+	if depth == 18:
+	    model = models.resnet18(pretrained)
+	elif depth == 50:
+	    model = models.resnet50(pretrained)
+
+        self.num_ftrs = model.fc.in_features
+        # self.num_classes = num_classes
+
+        self.shared = nn.Sequential(*list(model.children())[:-1])
+        self.target = nn.Sequential(nn.Linear(self.num_ftrs, num_classes))
+
+    def forward(self, x):
+        # pdb.set_trace()
+
+        x = self.shared(x)
+        x = torch.squeeze(x)
+        return self.target(x)
+
+    # def predict(self, x, k=3):
+    #     outputs = self.forward(x)
+    #     topk = np.argsort(outputs.data.cpu.numpy(), axis = 1)[:, -k:][:, ::-1]
+    #     return topk
+
+    # def predict_dir(self, data_dir, k = 3):
+    #     """
+    #     predict image in a folder
+    #     """
+    #     # Step 1: List all images in a folder with ext .jpg
+    #     fns = [data_dir + fn for fn in os.listdir(data_dir) if fn.endswith('.jpg')]
+    #     lbs = [-1]*len(fns)
+    #     data_loader = MyDataset(fns, lbs)
+
+    def frozen_until(self, to_layer):
+        print('Frozen shared part until %d-th layer, inclusive'%to_layer)
+
+        # if to_layer = -1, frozen all
+        child_counter = 0
+        for child in self.shared.children():
+            if child_counter <= to_layer:
+                print("child ", child_counter, " was frozen")
+                for param in child.parameters():
+                    param.requires_grad = False
+                # frozen deeper children? check
+                # https://spandan-madan.github.io/A-Collection-of-important-tasks-in-pytorch/
+            else:
+                print("child ", child_counter, " was not frozen")
+                for param in child.parameters():
+                    param.requires_grad = True
+            child_counter += 1
+
+
+def mytopk(pred, gt, k=3):
+    """
+    compute topk error
+    pred: (n_sample,n_class) np array
+    gt: a list of ground truth
+    --------
+    return:
+        n_correct: number of correct prediction 
+        topk_error: error, = n_connect/len(gt)
+    """
+    # topk = np.argpartition(pred, -k)[:, -k:]
+    topk = np.argsort(pred, axis = 1)[:, -k:][:, ::-1]
+    diff = topk - np.array(gt).reshape((-1, 1))
+    n_correct = np.where(diff == 0)[0].size 
+    topk_error = float(n_correct)/pred.shape[0]
+    return n_correct, topk_error
+
+def net_frozen(args, model):
+    print('********************************************************')
+    
+    model.frozen_until(args.frozen_until)
+    init_lr = args.lr
+    if args.trainer.lower() == 'adam':
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), 
+                lr=init_lr, weight_decay=args.weight_decay)
+    elif args.trainer.lower() == 'sgd':
+        optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), 
+                lr=init_lr,  weight_decay=args.weight_decay)
+    print('********************************************************')
+    return model, optimizer
+
+def parallelize_model(model):
+    if torch.cuda.is_available():
+        model = model.cuda()
+        model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+        cudnn.benchmark = True
+    return model
+
+def unparallelize_model(model):
+    try:
+        while 1:
+            # to avoid nested dataparallel problem
+            model = model.module
+    except AttributeError:
+        pass
+    return model
+
+def second2str(second):
+    h = int(second/3600.)
+    second -= h*3600.
+    m = int(second/60.)
+    s = int(second - m*60)
+    return "{:d}:{:02d}:{:02d} (s)".format(h, m, s)
+
+def print_eta(t0, cur_iter, total_iter):
+    """
+    print estimated remaining time
+    t0: beginning time
+    cur_iter: current iteration
+    total_iter: total iterations
+    """
+    time_so_far = time() - t0
+    iter_done = cur_iter + 1
+    iter_left = total_iter - cur_iter - 1
+    second_left = time_so_far/float(iter_done) * iter_left
+    s0 = 'Iter: '+ str(cur_iter + 1) + '/' + str(total_iter) + ', time so far: ' \
+        + second2str(time_so_far) + ', estimated time left: ' + second2str(second_left)
+    print(s0)
+
+def cvt_to_gpu(X):
+    return Variable(X.cuda()) if torch.cuda.is_available() \
+        else Variable(X)
